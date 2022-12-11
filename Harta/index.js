64eb7d92057c4180c92b104b056ab898afc27d71
Peter Popa <118959968+PopaPeter@users.noreply.github.com>
2022-12-11 05:13:20 +0000

    Puncte coordonate

--- Harta/index.js
+++ Harta/index.js
@@ -1,17 +1,32 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 44.4410873, lng: 26.0516349 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
+  // The location of Uluru
+  const coord_1 = {};
+  const coord_2 = {};
+  const coord_3 = {};
+  const user_coord = { lat: 44.4410873, lng: 26.0516349 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: user_coord,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: user_coord,
+    map: map,
+  });
+  const marker_1 = new google.maps.Marker({
+    position: coord_1,
+    map: map,
+  });
+  const marker_2 = new google.maps.Marker({
+    position: coord_2,
+    map: map,
+  });
+  const marker_3 = new google.maps.Marker({
+    position: coord_3,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
